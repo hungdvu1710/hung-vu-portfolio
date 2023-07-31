34eb92c3cda96092680b1b20f7bfb1d0a64ca78d
Hung Vu <hungvsme@gmail.com>
2023-07-31 12:59:32 -0500

    Implement Header

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,11 +1,23 @@
 import logo from "../assets/logo.png";
 import git from "../assets/git.png";
 import logoNoSlogan from "../assets/logo-no-slogan.png";
+import profile from "../assets/profile.png";
+import python from "../assets/python.png";
+import react from "../assets/react.png";
+import javascript from "../assets/javascript.png";
+import node from "../assets/node.png";
+import circle from "../assets/circle.svg";
 
 const images = {
   logo,
   git,
-  logoNoSlogan
-}
+  logoNoSlogan,
+  profile,
+  node,
+  javascript,
+  python,
+  react,
+  circle,
+};
 
-export default images
+export default images;
