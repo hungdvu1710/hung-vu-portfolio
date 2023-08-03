f0786025df5fb279524a3689127377207bef615a
Hung Vu <hungvsme@gmail.com>
2023-08-03 18:17:13 -0500

    Init Sanity

---
+++ sanity_backend/schemas/index.js
@@ -0,0 +1,7 @@
+import abouts from './abouts'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+import experiences from './experiences'
+
+export const schemaTypes = [abouts, skills, workExperience, contact, experiences]
