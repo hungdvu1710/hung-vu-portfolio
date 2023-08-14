ee62785f32c05928ddfe0ae0452257f17ed4202d
Hung Vu <hungvsme@gmail.com>
2023-08-13 22:50:23 -0500

    Deploy with Netlify

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -4,11 +4,13 @@ const NavigationDots = ({ active }) => (
   <div className="app__navigation">
     {["home", "about", "projects", "skills"].map(
       (item, index) => (
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
         <a
           href={`#${item}`}
           key={item + index}
           className="app__navigation-dot"
           style={active === item ? { backgroundColor: "#313BAC" } : {}}
+          aria-label={`Navigate to ${item} section`}
         />
       )
     )}
