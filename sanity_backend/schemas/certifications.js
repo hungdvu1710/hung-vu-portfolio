da9ed3feb55e2a542b9c44e11860a97f72780ec3
Hung Vu <hungvsme@gmail.com>
2023-10-11 15:17:52 -0500

    Add certification schema

---
+++ sanity_backend/schemas/certifications.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'certifications',
+  title: 'Certifications',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'link',
+      title: 'Link',
+      type: 'string',
+    }
+  ],
+}
--- sanity_backend/schemas/index.js
+++ sanity_backend/schemas/index.js
@@ -3,5 +3,6 @@ import skills from './skills'
 import workExperience from './workExperience'
 import experiences from './experiences'
 import projects from './projects'
+import certifications from './certifications'
 
-export const schemaTypes = [abouts, skills, workExperience, experiences, projects]
+export const schemaTypes = [abouts, skills, workExperience, experiences, projects, certifications]
